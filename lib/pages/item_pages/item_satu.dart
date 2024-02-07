a798be05a64a7304ffab3e35eb15cc08bf9c5bfc
Fitrianto Dwi Cahyo <riandwicahyo28@gmail.com>
2024-02-07 11:16:49 +0700

    chore: add assets for in the app

---
+++ lib/pages/item_pages/item_satu.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ItemSatu extends StatelessWidget {
+  const ItemSatu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
