fcfacaa135909f7a685deab976e5652dff6cb17b
Fitrianto Dwi Cahyo <riandwicahyo28@gmail.com>
2024-02-21 11:25:50 +0700

    feat(app_color): menambahkan app_color.dart

---
+++ lib/theme/color/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryBg = Color(0xffFFFFFF);
+
+  static const Color primaryElement = Color(0xffB9FF66);
+  static const Color secondaryElement = Color(0xff191A23);
+  static const Color treeElement = Color(0xffF3F3F3);
+
+  static const Color primaryTextColor = Color(0xff000000);
+}
